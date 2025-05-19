949781d40bc30c6660e4f979b3145c7bd4c54fff
likhi_g <183710719+likhithag0328@users.noreply.github.com>
2025-05-19 10:54:44 +0530

    changes names of tests

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
     Rect rectangle = new Rect(7,2);
     int area = rectangle.area();
-    
+
     System.out.println(area);
     }
 
--- test/com/tw/step10/RectTest.java
+++ test/com/tw/step10/RectTest.java
@@ -6,11 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RectTest {
     @Test
-    void area() {
-        Rect rect1 = new Rect(4,5);
-        assertEquals(rect1.area(),20);
+    void areaTestForValidRect() {
+        Rect rectangle = new Rect(4,5);
+        assertEquals(rectangle.area(),20);
+    }
 
-        Rect rect2 = new Rect(0,5);
-        assertEquals(rect2.area(),0);
+    @Test
+    void areaTestForInvalidRect() {
+        Rect rectangle = new Rect(0,5);
+        assertEquals(rectangle.area(),0);
     }
 }
